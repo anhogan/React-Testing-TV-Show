746b29e28a74b4288f089246a4d37ca0f3151fff
Amanda Hogan <ahogan@teamworks.com>
2020-02-24 18:49:10 -0500

    Add in tests

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -4,5 +4,8 @@ export const fetchShow = () => {
   return axios.get('https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes')
     .then(res => {
       return res
+    })
+    .catch(err => {
+      return err
     });
 };
